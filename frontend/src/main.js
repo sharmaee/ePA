daf06f50b5a847e24303adbd1d62f6eb164a2af2
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-07-11 17:07:26 +0300

    add formKit settings, test form in HomePage template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import { createHead } from "@vueuse/head";
+import { plugin, defaultConfig } from "@formkit/vue";
+import "@formkit/themes/genesis";
 
-createApp(App).use(router).use(createHead()).mount("#app");
+createApp(App).use(router).use(createHead()).use(plugin, defaultConfig).mount("#app");
