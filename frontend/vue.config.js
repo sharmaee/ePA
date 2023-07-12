1aa8b6cee824778d169842827b8fba002bd2ba9a
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-07-12 15:29:32 +0300

    add services folder, update vue.config file

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,22 +2,39 @@
 const path = require("path");
 const { gitDescribeSync } = require("git-describe");
 
+const CKEditorWebpackPlugin = require("@ckeditor/ckeditor5-dev-webpack-plugin");
+const { styles } = require("@ckeditor/ckeditor5-dev-utils");
+
 const gitInfo = gitDescribeSync();
 
 process.env.VUE_APP_VERSION = gitInfo.distance ? gitInfo.raw : gitInfo.tag;
 
 module.exports = {
   outputDir: "../frontend_assets",
-  transpileDependencies: true,
+  assetsDir: "static",
 
   css: {
     loaderOptions: {
       sass: {
-        additionalData: `@import "~@/styles/variables"`,
+        additionalData: `@import "~@/styles/global-variables"`,
       },
       scss: {
-        additionalData: `@import "~@/styles/variables";`,
+        additionalData: `@import "~@/styles/global-variables";`,
       },
     },
-  }
+  },
+  
+  devServer: {
+    proxy: {
+      "/api*": {
+        // Forward frontend dev server request for /api to django dev server
+        target: "http://localhost:8000/",
+      },
+      "/admin-*": {
+        // Forward frontend dev server request for /api to django dev server
+        target: "http://localhost:8000/",
+      },
+    },
+  },
+
 };
