d9fcc4e7dc6cfbc45ec8f3ca134ecd8db7fb26cb
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-07-12 14:49:49 +0300

    Fix routes accessing

--- backend/portal/api/requirements/views.py
+++ backend/portal/api/requirements/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework import views
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
 
 from portal.exceptions import PortalException
 
@@ -10,6 +12,8 @@ from portal.logic.search.search_requirements import run_search
 
 
 class PriorAuthRequirementsView(views.APIView):
+    permission_classes = (AllowAny,)
+
     def get(self, request):
         requirements = PriorAuthRequirement.objects.all()
         result = PriorAuthRequirementSerializer(requirements, many=True).data
@@ -17,6 +21,8 @@ class PriorAuthRequirementsView(views.APIView):
 
 
 class PriorAuthRequirementSearchView(views.APIView):
+    permission_classes = (AllowAny,)
+
     def post(self, request):
         requirements = run_search(request.data)
         result = PriorAuthRequirementSerializer(requirements, many=True).data
