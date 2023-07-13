039b70d2430148b533844040cdb83a609e8f2d37
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-07-13 14:42:26 +0300

    [WIP] styling

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,11 +1,14 @@
 <template>
   <PriorHeader />
-  <div class="home-page-content">
-    <div class="left-content">
-      <h2>Neque porro <span>Auisquam</span> est hui dolorem ipsum.</h2>
-    </div>
-    <div class="right-content">
-      <h2>Start a New Request</h2>
+  <div class="home-page-wrapper">
+    <h1>The <span class="lilac-text">Wegovy</span> Insurance Navigator.</h1>
+    <div class="home-page-main-block">
+      <div class="home-page-img">
+        <img src="../assets/images/man.png" alt="man" />
+      </div>
+      <div class="home-page-form">
+        <h2>Start a New Request</h2>
+      </div>
     </div>
   </div>
   <PriorFooter />
@@ -28,23 +31,5 @@ async function getAvailableSearchOptions() {
 </script>
 
 <style lang="sass" scoped>
-
-.home-page-content
-  display: flex
-
-  .left-content
-    width: 50%
-    display: flex
-    align-items: center
-    justify-content: center
-
-    h2
-      font-size: 64px
-      font-weight: 700
-      width: 90%
-
-      span
-        color: #556FF3
-  .right-content
-    width: 50%
+@import "../styles/pages/_home-page"
 </style>
