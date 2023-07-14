b5fdb6e99a13e96b85935dc3288dec81b40ed5e3
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-07-14 22:20:28 +0300

    Add trigram similar search, add plans by provider search options

--- backend/portal/logic/search/search_requirements.py
+++ backend/portal/logic/search/search_requirements.py
@@ -2,18 +2,25 @@ from portal.models import PriorAuthRequirement
 
 
 def run_search(search_params):
+    search_params = {k + '__trigram_similar': v for k, v in search_params.items() if v is not None}
     return PriorAuthRequirement.objects.filter(**search_params)
 
-
+ 
 def get_available_search_options():
     available = PriorAuthRequirement.objects.all()
     insurance_providers = list(available.values_list('insurance_provider', flat=True).distinct())
     insurance_plan_numbers = list(available.values_list('insurance_plan_number', flat=True).distinct())
     insurance_coverage_states = list(available.values_list('insurance_coverage_state', flat=True).distinct())
     medications = list(available.values_list('medication', flat=True).distinct())
+    insurance_plans_by_provider = {}
+    for provider in insurance_providers:
+        insurance_plans_by_provider[provider.lower()] = list(
+            available.filter(insurance_provider=provider).values_list('insurance_plan_number', flat=True).distinct()
+        )
     return {
         "insurance_providers": insurance_providers,
         "insurance_plan_numbers": insurance_plan_numbers,
         "insurance_coverage_states": insurance_coverage_states,
         "medications": medications,
+        "insurance_plans_by_provider": insurance_plans_by_provider,
     }
--- backend/portal/models/requirements.py
+++ backend/portal/models/requirements.py
@@ -1,11 +1,11 @@
 from django.db import models
-
 from ._common import PortalModelBase
 
 
 class PriorAuthRequirement(PortalModelBase):
-    insurance_provider = models.TextField(blank=True, null=True)
-    insurance_plan_number = models.TextField(blank=True, null=True)
-    insurance_coverage_state = models.TextField(blank=True, null=True)
-    medication = models.TextField(blank=True, null=True)
+    insurance_provider = models.TextField(blank=True, null=True, db_index=True)
+    insurance_plan_number = models.TextField(blank=True, null=True, db_index=True)
+    insurance_coverage_state = models.TextField(blank=True, null=True, db_index=True)
+    medication = models.TextField(blank=True, null=True, db_index=True)
     requirements_flow = models.TextField(blank=True, null=True)
+    
