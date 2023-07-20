2816d910cc65889a82ff0091eed14d20c4741a9a
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-07-20 17:13:42 +0300

    add analog to graph

--- frontend/src/pages/FlowRequirementsPage.vue
+++ frontend/src/pages/FlowRequirementsPage.vue
@@ -1,18 +1,28 @@
 <template>
   <PriorHeader />
-  <div id="graph-page-wrapper" class="graph-page-wrapper">
+  <div class="graph-page-wrapper">
     <h1>The Wegovy <span class="blue-text">Insurance</span> Navigator</h1>
     <p>
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id es Sed ut
       perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, to
     </p>
+
+    <button v-if="!preloader" class="switch-map" @click="mapSwitcher">{{ showMap }}</button>
+
     <GreenCirclePreloader v-if="preloader" />
+
+    <div id="graph-wrapper" :class="{ hidden: showMap === 'analog' }" class="graph-wrapper"></div>
+
+    <div :class="{ hidden: showMap === 'graph' }" class="questionaire-wrapper">
+      <RecursiveComponent :data="parsedData" />
+    </div>
   </div>
+
   <PriorFooter />
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import { instance } from "@viz-js/viz";
 import { useRoute } from "vue-router";
 import { mainServices } from "@/services/mainServices";
@@ -20,15 +30,17 @@ import { mainServices } from "@/services/mainServices";
 import PriorFooter from "@/components/PriorFooter";
 import PriorHeader from "@/components/PriorHeader";
 import GreenCirclePreloader from "@/components/GreenCirclePreloader";
+import RecursiveComponent from "@/pages/RecursiveComponent";
 
 const graphContainer = ref(null);
 const route = useRoute();
+const showMap = ref("graph");
 
 const graphData = ref(null);
 const preloader = ref(false);
 
 onMounted(() => {
-  graphContainer.value = document.getElementById("graph-page-wrapper");
+  graphContainer.value = document.getElementById("graph-wrapper");
   if (route.params.id) {
     getPriorAuthRequirements(route.params.id);
   }
@@ -47,6 +59,188 @@ async function getPriorAuthRequirements(id) {
 
   preloader.value = false;
 }
+function mapSwitcher() {
+  showMap.value = showMap.value === "graph" ? "analog" : "graph";
+  console.log(showMap.value);
+}
+
+const parsedData = reactive({
+  class: "InteractiveSelect",
+  legend: "Start or continue",
+  type: "fieldset",
+  options: [
+    {
+      class: "InteractiveOption",
+      label: "Start",
+      type: "radio",
+      state: false,
+      children: [
+        {
+          class: "InteractiveSelect",
+          legend: "Age of patient starting therapy",
+          type: "fieldset",
+          options: [
+            {
+              class: "InteractiveOption",
+              label: "Age 12 to 17 y.o.",
+              type: "radio",
+              state: false,
+              children: [
+                {
+                  class: "CheckListQuestion",
+                  question: "Treatment is being requested for appetite suppression or weight loss",
+                  type: "checkbox",
+                  value: false,
+                },
+                {
+                  class: "CheckListQuestion",
+                  question: "One of the following",
+                  type: "checkbox",
+                  value: false,
+                  subchecklist: [
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or: "Body Mass Index (BMI) ≥ 30 kg/m2 or for pediatric patients a BMI > 95th percentile",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or:
+                        "BMI ≥ 27 kg/m2 and patient has a weight-related comorbidity (e.g., dyslipidemia, hypertension, type 2 diabetes, sleep apnea)",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              class: "InteractiveOption",
+              label: "Age >18 y.o.",
+              type: "radio",
+              state: false,
+              children: [
+                {
+                  class: "CheckListQuestion",
+                  question: "Treatment is being requested for appetite suppression or weight loss",
+                  type: "checkbox",
+                  value: false,
+                },
+                {
+                  class: "CheckListQuestion",
+                  question: "One of the following",
+                  type: "checkbox",
+                  value: false,
+                  subchecklist: [
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or: "Body Mass Index (BMI) ≥ 30 kg/m2 or for pediatric patients a BMI > 95th percentile",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or:
+                        "BMI ≥ 27 kg/m2 and patient has a weight-related comorbidity (e.g., dyslipidemia, hypertension, type 2 diabetes, sleep apnea)",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      class: "InteractiveOption",
+      label: "Continue",
+      type: "radio",
+      state: false,
+      children: [
+        {
+          class: "InteractiveSelect",
+          legend: "Age of patient continuing therapy",
+          type: "fieldset",
+          options: [
+            {
+              class: "InteractiveOption",
+              label: "Age 12 to 17 y.o.",
+              state: false,
+              type: "radio",
+              children: [
+                {
+                  class: "CheckListQuestion",
+                  question: "Treatment is being requested for appetite suppression or weight loss",
+                  type: "checkbox",
+                  value: false,
+                },
+                {
+                  class: "CheckListQuestion",
+                  question: "One of the following",
+                  type: "checkbox",
+                  value: false,
+                  subchecklist: [
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or: "Body Mass Index (BMI) ≥ 30 kg/m2 or for pediatric patients a BMI > 95th percentile",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or:
+                        "BMI ≥ 27 kg/m2 and patient has a weight-related comorbidity (e.g., dyslipidemia, hypertension, type 2 diabetes, sleep apnea)",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              class: "InteractiveOption",
+              label: "Age >18 y.o.",
+              state: false,
+              type: "radio",
+              children: [
+                {
+                  class: "CheckListQuestion",
+                  question: "Treatment is being requested for appetite suppression or weight loss",
+                  type: "checkbox",
+                  value: false,
+                },
+                {
+                  class: "CheckListQuestion",
+                  question: "One of the following",
+                  type: "checkbox",
+                  value: false,
+                  subchecklist: [
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or: "Body Mass Index (BMI) ≥ 30 kg/m2 or for pediatric patients a BMI > 95th percentile",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                    {
+                      class: "SubCheckListQuestion",
+                      question_or:
+                        "BMI ≥ 27 kg/m2 and patient has a weight-related comorbidity (e.g., dyslipidemia, hypertension, type 2 diabetes, sleep apnea)",
+                      type: "checkbox",
+                      value_or: false,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
 </script>
 
 <style lang="sass" scoped>
---
+++ frontend/src/pages/RecursiveComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div>
+      <div v-if="parseData.class === 'InteractiveSelect'">
+        <fieldset>
+          <legend>{{ parseData.legend }}</legend>
+          <div v-for="option in parseData.options" :key="option.label">
+            <RecursiveComponent :data="option" />
+          </div>
+        </fieldset>
+      </div>
+
+      <div v-else-if="parseData.class === 'InteractiveOption'">
+        <div>
+          <h3>{{ parseData.label }}</h3>
+          <input v-model="parseData.state" type="radio" />
+          <div v-if="parseData.state">
+            <div v-for="child in parseData.children" :key="child.label">
+              <RecursiveComponent :data="child" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-else-if="data.class === 'CheckListQuestion'">
+        <div>
+          <h4>{{ data.question }}</h4>
+          <input v-model="parseData.value" type="checkbox" />
+
+          <div v-if="parseData.subchecklist">
+            <div v-for="subQuestion in parseData.subchecklist" :key="subQuestion.question_or">
+              <RecursiveComponent :data="subQuestion" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-else-if="parseData.class === 'SubCheckListQuestion'">
+        <div>
+          <span>--- {{ parseData.question_or }}</span>
+          <input v-model="parseData.value_or" type="checkbox" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+import RecursiveComponent from "@/pages/RecursiveComponent";
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+
+const parseData = ref({});
+// eslint-disable-next-line vue/no-setup-props-destructure
+parseData.value = props.data;
+</script>
+
+<style lang="scss" scoped></style>
