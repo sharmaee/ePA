9ac4863e821bb4864cb1b5c132e21dd596dc4a9a
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-07-21 10:03:16 +0300

    Refactor to use 2 classes of InteractiveSelect and SelectOption

--- frontend/src/pages/RecursiveComponent.vue
+++ frontend/src/pages/RecursiveComponent.vue
@@ -3,42 +3,29 @@
     <div>
       <div v-if="parseData.class === 'InteractiveSelect'">
         <fieldset>
-          <legend>{{ parseData.legend }}</legend>
-          <div v-for="option in parseData.options" :key="option.label">
+          <legend>{{ parseData.label }}</legend>
+          <div v-for="option in parseData.children" :key="option.label">
             <RecursiveComponent :data="option" />
           </div>
         </fieldset>
       </div>
 
-      <div v-else-if="parseData.class === 'InteractiveOption'">
+      <div v-else-if="parseData.class === 'SelectOption'">
         <div>
           <h3>{{ parseData.label }}</h3>
-          <input v-model="parseData.state" type="radio" />
-          <div v-if="parseData.state">
+          <input v-model="parseData.value" :type="parseData.type" />
+          <div v-if="parseData.type === 'radio' && parseData.value">
             <div v-for="child in parseData.children" :key="child.label">
               <RecursiveComponent :data="child" />
             </div>
           </div>
-        </div>
-      </div>
-
-      <div v-else-if="data.class === 'CheckListQuestion'">
-        <div>
-          <h4>{{ data.question }}</h4>
-          <input v-model="parseData.value" type="checkbox" />
-
-          <div v-if="parseData.subchecklist">
-            <div v-for="subQuestion in parseData.subchecklist" :key="subQuestion.question_or">
-              <RecursiveComponent :data="subQuestion" />
+          <div>
+            <div v-if="parseData.type === 'checkbox'">
+              <div v-for="child in parseData.children" :key="child.label">
+                <RecursiveComponent :data="child" />
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div v-else-if="parseData.class === 'SubCheckListQuestion'">
-        <div>
-          <span>{{ parseData.question_or }}</span>
-          <input v-model="parseData.value_or" type="checkbox" />
         </div>
       </div>
     </div>
