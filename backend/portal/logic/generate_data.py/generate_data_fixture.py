7d977b074ebf61d29e5e4cfb67eea80a74a3b31a
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-08-08 14:29:35 +0300

    Start working on fixture generator

---
+++ backend/portal/logic/generate_data.py/generate_data_fixture.py
@@ -0,0 +1,10 @@
+# connect to file in s3
+# load file into memory
+# create list of objects to save
+# for each line in file:
+    # split line into chunks
+    # generate url slug
+    # create PriorAuthRequirement object
+    # add to list of objects to save
+# save all objects
+
