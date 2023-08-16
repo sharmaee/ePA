2b6647a076a5fa8e075187690417df59d77906e6
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-08-16 15:09:21 +0300

    Update form field names, update images, update form formatting

--- frontend/src/pages/RequestMissingRequirements.vue
+++ frontend/src/pages/RequestMissingRequirements.vue
@@ -2,7 +2,7 @@
   <PriorHeader />
   <div class="missing-requirements-wrapper">
     <h1 :class="{ hide: screenWidth < 835 }">Request Call for Critera</h1>
-    <p>Our Agents are On Stand-By to Find what you need to submit. Current expected Turnaround: x</p>
+    <p>Our Agents are On Stand-By to Find what you need to submit.<br />Current expected Turnaround: &lt; 24 hours</p>
     <div class="shadow-ellipse shadow-ellipse-right"></div>
     <div class="request-missing-requirements-form-wrapper">
       <div class="missing-requirements-form">
@@ -10,13 +10,13 @@
         <hr />
         <form action="">
           <div class="row-with-two-input">
-            <div class="patient-first-name">
-              <label for="patient-first-name">Patient First Name</label>
-              <input id="patient-first-name" v-model="data.first_name" type="text" placeholder="eg. John" />
+            <div class="cover-my-meds-key">
+              <label for="cover-my-meds-key">CoverMyMeds Key</label>
+              <input id="cover-my-meds-key" v-model="data.coverMyMedsKey" type="text" placeholder="eg. B4HL4T2E" />
             </div>
             <div class="patient-last-name">
               <label for="patient-last-name">Patient Last Name</label>
-              <input id="patient-last-name" v-model="data.last_name" type="text" placeholder="eg. Snow" />
+              <input id="patient-last-name" v-model="data.lastName" type="text" placeholder="eg. Snow" />
             </div>
           </div>
 
@@ -30,41 +30,14 @@
             </div>
             <div class="patient-member-id">
               <label for="patient-member-id">Patient Member ID</label>
-              <input id="patient-member-id" v-model="data.member_id" type="text" placeholder="eg. H3485045" />
+              <input id="patient-member-id" v-model="data.memberId" type="text" placeholder="eg. H3485045" />
             </div>
           </div>
 
           <div class="row-with-one-input">
-            <label for="patient-phone-number">Patient Phone Number</label>
-            <input
-              id="patient-phone-number"
-              v-model="data.phone_number"
-              type="text"
-              placeholder="eg. +1-XXX-XXX-XXXX" />
-          </div>
-
-          <div class="row-with-one-input">
-            <label for="patient-plan-type">Patient Plan Type</label>
-            <input
-              id="patient-plan-type"
-              v-model="insurance_plan_type"
-              type="text"
-              placeholder="eg. Medicare, Commercial, Medicaid " />
-          </div>
-
-          <div class="row-with-one-input">
-            <label for="patient-address">Patient Address</label>
-            <input id="patient-address" v-model="data.address" type="text" placeholder="eg. 541 Addison Ave, Palo " />
-          </div>
-
-          <div class="row-with-two-input">
-            <div class="referring-doctor">
-              <label for="referring-doctor">Referring Doctor</label>
-              <input id="referring-doctor" v-model="data.referring_doctor" type="text" placeholder="eg. Aetna" />
-            </div>
             <div class="your-email">
               <label for="your-email">Your Email</label>
-              <input id="your-email" v-model="data.email" type="text" placeholder="example@findsunrise.com" />
+              <input id="your-email" v-model="data.maEmail" type="text" placeholder="example@findsunrise.com" />
             </div>
           </div>
           <span class="agreement">
@@ -74,9 +47,10 @@
             contact with: applicable health insurer(s); and any pharmacy involved in my (or the patient's) treatment. I
             understand that I do not need to sign this form in order to obtain treatment, insurance, or insurance
             benefits; I am required to sign it only if I wish to receive optional Lamar Health support. I understand I
-            can revoke my authorization at any time by emailing my revocation to security@lamarhealth.com.
+            can revoke my authorization at any time by emailing my revocation to
+            <a href="mailto:security@lamarhealth.com">security@lamarhealth.com</a>.
           </span>
-          <button @click="sendRequirements">Email me info</button>
+          <button @click="sendRequirements">Email me steps</button>
         </form>
       </div>
     </div>
@@ -86,7 +60,7 @@
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import PriorHeader from "@/components/PriorHeader";
 import PriorFooter from "@/components/PriorFooter";
 import { mainServices } from "@/services/mainServices";
@@ -100,19 +74,13 @@ const screenWidth = ref(null);
 
 const data = ref({
   medication: mainFormData.medication,
-  insurance_provider: mainFormData.insuranceProvider,
-  insurance_coverage_state: mainFormData.insuranceCoverageState,
-  first_name: "",
-  last_name: "",
+  insuranceProvider: mainFormData.insuranceProvider,
+  insuranceCoverageState: mainFormData.insuranceCoverageState,
+  lastName: "",
   dob: "",
-  email: "",
-  member_id: "",
-  phone_number: "",
-  address: "",
-  insurance_plan_type: "",
-  referring_doctor: "",
-  ma_email: "",
-  release_version: "",
+  memberId: "",
+  maEmail: "",
+  releaseVersion: "0.0.1",
 });
 
 function displayWindowSize() {
