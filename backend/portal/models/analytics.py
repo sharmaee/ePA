14ab8a3ef2af8690b0419acb07c97ddfa63cd36a
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-09-19 18:22:47 +0300

    Add proxy

--- backend/portal/models/analytics.py
+++ backend/portal/models/analytics.py
@@ -8,3 +8,11 @@ class RequirementsSearchAction(PortalModelBase):
     medication = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class RequirementsSearchActionSummary(RequirementsSearchAction):
+    class Meta:
+        proxy = True
+        verbose_name_plural = 'Requirements Search Action Summary'
+        verbose_name = 'Requirements Search Action Summary'
+        ordering = ['-created_on']
