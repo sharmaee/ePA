8ff8fa10bb4ef2efe28d9576d6407a1ef1a55688
Liudmyla Ikonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2023-09-19 13:31:41 +0300

    Set refresh lifetime to 1 day

--- backend/portal/settings/base.py
+++ backend/portal/settings/base.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 # Static files (CSS, JavaScript, Images)
